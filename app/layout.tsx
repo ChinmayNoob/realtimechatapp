0f037a0b9875eb8c5c07342b186bf93af91cd6e5
ChinmayNoob <chinmaysk0408@gmail.com>
2024-05-10 01:51:56 +0530

    create message functionality done added crud operation in the input

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Space_Grotesk } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider"
+import { Toaster } from "@/components/ui/sonner";
 const space_Grotesk = Space_Grotesk({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -24,6 +25,8 @@ export default function RootLayout({
             disableTransitionOnChange
           >
             {children}
+            <Toaster position="top-center"/>
+
           </ThemeProvider>
         </body>
     </html>
