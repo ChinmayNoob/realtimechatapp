3d9ea19010e3302224fc4be00d0a2529343990d9
ChinmayNoob <chinmaysk0408@gmail.com>
2024-05-09 17:53:23 +0530

    completed supabase connection and initializing zustand state management

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,17 @@
-import React from 'react'
-import { Button } from '@/components/ui/button'
-export default function page() {
-  return (
-    <div className='max-w-3xl mx-auto md:py-10 h-screen'>
-      <div className="h-full border rounded-md">
-        <div className='h-20'>
-          <div className='p-5 border-b flex items-center justify-between'>
-            <div>
-              <h1 className='text-xl font-bold'>
-                Live Chat
-              </h1>
-              <div className='flex items-center gap-x-1'>
-                <div className='h-4 w-4 bg-green-500 rounded-full'></div>
-                <h2 className='text-sm text-gray-300'>3 online</h2>
-              </div>
-            </div>
-            <Button className='text-bold text-white'>
-              Login
-            </Button>
-          </div>
-        </div>
-      </div>
-    </div>
-  )
+import React from "react";
+import ChatHeader from "@/components/ChatHeader";
+import { supabaseServer } from "@/lib/supabase/server";
+export default async function Page() {
+	const supabase = await supabaseServer();
+	const { data } = await supabase.auth.getSession();
+
+	return (
+		<>
+			<div className="max-w-3xl mx-auto md:py-10 h-screen">
+				<div className=" h-full border rounded-md flex flex-col relative">
+					<ChatHeader user={data.session?.user} />
+				</div>
+			</div>
+		</>
+	);
 }
